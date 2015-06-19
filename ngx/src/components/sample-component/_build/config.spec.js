e9054dde0ed20573ab00aed7dbaf2bbf18ffbf9b
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-19 17:30:17 +0200

    feat(sample-component): add component

---
+++ ngx/src/components/sample-component/_build/config.spec.js
@@ -0,0 +1,9 @@
+import config from './config';
+
+describe('config', function(){
+
+  it('should exist', function(){
+    expect(config).to.be.an('object');
+  });
+
+});
---
+++ ngx/src/components/sample-component/_build/index.js
@@ -0,0 +1,13 @@
+import angular from "angular";
+import config from "./config";
+
+export default function(ngModule, options){
+  angular.merge(config, options);
+  ngModule.run(logMessage);
+};
+
+function logMessage($log) {
+  $log.info('Sample component is working!');
+}
+
+logMessage.$inject = ['$log'];
