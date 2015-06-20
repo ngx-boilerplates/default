2ca0cc9f1f6c5053898dffbc8e5bcf1ea54b0803
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-20 17:23:06 +0200

    refactor(js): replace double quotes with single quotes in import statements

--- ngx/src/app.js
+++ ngx/src/app.js
@@ -1,4 +1,4 @@
-import angular from "angular";
+import angular from 'angular';
 import c1 from 'components/sample-component/_build/index';
 
 /**************************************************************************
--- ngx/src/components/sample-component/_build/index.js
+++ ngx/src/components/sample-component/_build/index.js
@@ -1,5 +1,5 @@
-import angular from "angular";
-import config from "./config";
+import angular from 'angular';
+import config from './config';
 
 export default function(ngModule, options){
   angular.merge(config, options);
